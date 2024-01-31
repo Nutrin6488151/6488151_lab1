4432f003da82ae87dc80a2e3d288f1cc7c09454f
Nutrin6488151 <nutrinpongjinda@gmail.com>
2024-01-31 22:39:32 +0700

    6488151Exercise 1 product_app

---
+++ lib/main.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Product layout demo home page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Product Listing")),
+        body: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
+          children: <Widget>[
+            ProductBox(
+                name: "Pizza",
+                description: "italian food ever",
+                price: 1000,
+                image: "pizza.jpg"),
+            ProductBox(
+                name: "Sausage",
+                description: "meat sausage for sausage lover",
+                price: 800,
+                image: "sausage.jpg"),
+            ProductBox(
+                name: "Ice-cream",
+                description: "vanilla ice cream",
+                price: 2000,
+                image: "ice.jpg"),
+            ProductBoxURL(
+                name: "BBQ",
+                description: "smoky bbq",
+                price: 1500,
+                image: "https://pngimg.com/d/barbecue_PNG14.png"),
+            ProductBoxURL(
+                name: "Smoothies",
+                description: "berries yogurt smoothies",
+                price: 100,
+                image: "https://i.ytimg.com/vi/TsFpCObjzOk/maxresdefault.jpg"),
+            ProductBoxURL(
+                name: "Burger",
+                description: "Cheese burger",
+                price: 20,
+                image:
+                    "https://www.foodandwine.com/thmb/DI29Houjc_ccAtFKly0BbVsusHc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/crispy-comte-cheesburgers-FT-RECIPE0921-6166c6552b7148e8a8561f7765ddf20b.jpg"),
+          ],
+        ));
+  }
+}
+
+class ProductBox extends StatelessWidget {
+  ProductBox(
+      {Key? key,
+      required this.name,
+      required this.description,
+      required this.price,
+      required this.image})
+      : super(key: key);
+  final String name;
+  final String description;
+  final int price;
+  final String image;
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(2),
+        height: 120,
+        child: Card(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+              Image.asset("assets/" + image),
+              Expanded(
+                  child: Container(
+                      padding: EdgeInsets.all(5),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Text(this.name,
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text(this.description),
+                          Text("Price: " + this.price.toString()),
+                        ],
+                      )))
+            ])));
+  }
+}
+
+class ProductBoxURL extends StatelessWidget {
+  ProductBoxURL(
+      {Key? key,
+      required this.name,
+      required this.description,
+      required this.price,
+      required this.image})
+      : super(key: key);
+  final String name;
+  final String description;
+  final int price;
+  final String image;
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(2),
+        height: 120,
+        child: Card(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+              Image.network(image),
+              Expanded(
+                  child: Container(
+                      padding: EdgeInsets.all(5),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Text(this.name,
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text(this.description),
+                          Text("Price: " + this.price.toString()),
+                        ],
+                      )))
+            ])));
+  }
+}
